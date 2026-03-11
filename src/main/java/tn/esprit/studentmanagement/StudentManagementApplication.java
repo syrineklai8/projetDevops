21d0c8e83578ca0245fa9f24b62326f047243fd5
syrineklai8 <150437527+syrineklai8@users.noreply.github.com>
2026-03-11 13:45:43 +0100

    Introduced compile error by calling unknown method

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,5 +1,1 @@
-public class StudentManagementApplication {
-    public static void main(String[] args) {
-        System.out.println(undefinedVariable);
-    }
-}
+public class StudentManagementApplication { public static void main(String[] args) { System.out.println(unknownMethod()); } }

faa008cb102070163c2d88cca4d8efae95c415ab
syrineklai8 <150437527+syrineklai8@users.noreply.github.com>
2026-03-11 13:01:26 +0100

    Introduced undefined variable to cause compile error

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,5 +1,5 @@
 public class StudentManagementApplication {
     public static void main(String[] args) {
-        System.out.println(1 / 0);
+        System.out.println(undefinedVariable);
     }
 }

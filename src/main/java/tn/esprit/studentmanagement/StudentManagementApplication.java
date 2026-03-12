0e1fd719d7836206cba722e18cd75b209b8a7199
syrineklai8 <150437527+syrineklai8@users.noreply.github.com>
2026-03-12 10:03:03 +0100

    Introduced compile error by calling unknown method

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,6 +1,1 @@
-public class StudentManagementApplication {
-    public static void main(String[] args) {
-        System.out.println("This will cause a test failure");
-        throw new RuntimeException("Test failure");
-    }
-}
+public class StudentManagementApplication { public static void main(String[] args) { System.out.println(unknownMethod()); } }

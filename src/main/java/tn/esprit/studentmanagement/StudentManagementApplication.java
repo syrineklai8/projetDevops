28404065a3e9a3c89990982d137369bc062d5ec3
syrineklai8 <150437527+syrineklai8@users.noreply.github.com>
2026-03-12 11:26:21 +0100

    Introduced division by zero error in main method

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,6 +1,5 @@
 public class StudentManagementApplication {
     public static void main(String[] args) {
-        System.out.println("This will cause a test failure");
-        throw new RuntimeException("Test failure");
+        System.out.println(1 / 0);
     }
 }

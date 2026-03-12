394589a6c374ff7af913415e7abb5d1d0fd35aba
syrineklai8 <150437527+syrineklai8@users.noreply.github.com>
2026-03-12 09:55:16 +0100

    Introduced test failure in StudentManagementApplication

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -1,5 +1,6 @@
 public class StudentManagementApplication {
     public static void main(String[] args) {
-        System.out.println(10 / 0);
+        System.out.println("This will cause a test failure");
+        throw new RuntimeException("Test failure");
     }
 }
